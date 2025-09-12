9d19756f146ac704b43704a03270c574c5b8948b
ngu <nguivo9@gmail.com>
2025-09-12 03:02:33 +0100

    refactor code so that Reusable card widget handles its own onTap event

--- lib/input_dart.dart
+++ lib/input_dart.dart
@@ -40,29 +40,25 @@ class _InputPageState extends State<InputPage> {
             child: Row(
               children: [
                 Expanded(
-                  child: GestureDetector(
-                    onTap: () {
+                  child: ReusableCard(
+                    onPress: () {
                       setState(() {
                         selectedGender = Gender.male;
                       });
                     },
-                    child: ReusableCard(
-                      bgColor: selectedGender == Gender.male ? activeCardColor : inactiveCardColor,
-                      cardChild: IconContent(icon: FontAwesomeIcons.mars, label: 'MALE'),
-                    ),
+                    bgColor: selectedGender == Gender.male ? activeCardColor : inactiveCardColor,
+                    cardChild: IconContent(icon: FontAwesomeIcons.mars, label: 'MALE'),
                   ),
                 ),
                 Expanded(
-                  child: GestureDetector(
-                    onTap: () {
+                  child: ReusableCard(
+                    onPress: () {
                       setState(() {
                         selectedGender = Gender.female;
                       });
                     },
-                    child: ReusableCard(
-                      bgColor: selectedGender == Gender.female ? activeCardColor : inactiveCardColor,
-                      cardChild: IconContent(icon: FontAwesomeIcons.venus, label: 'FEMALE'),
-                    ),
+                    bgColor: selectedGender == Gender.female ? activeCardColor : inactiveCardColor,
+                    cardChild: IconContent(icon: FontAwesomeIcons.venus, label: 'FEMALE'),
                   ),
                 ),
               ],
--- lib/reusable_card.dart
+++ lib/reusable_card.dart
@@ -1,20 +1,25 @@
 import 'package:flutter/material.dart';
 
 class ReusableCard extends StatelessWidget {
+  ReusableCard({required this.bgColor, required this.cardChild, this.onPress});
+
   final Color bgColor;
   final Widget cardChild;
+  GestureTapCallback? onPress;
 
-  ReusableCard({required this.bgColor, required this.cardChild}); // curly brackets make the parameter 'named'
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(15.0),
-      decoration: BoxDecoration(
-        color: bgColor,
-        borderRadius: BorderRadius.circular(10.0),
+    return GestureDetector(
+      onTap: onPress,
+      child: Container(
+        margin: EdgeInsets.all(15.0),
+        decoration: BoxDecoration(
+          color: bgColor,
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: cardChild,
       ),
-      child: cardChild,
     );
   }
 }
