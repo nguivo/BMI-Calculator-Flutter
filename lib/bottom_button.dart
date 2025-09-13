463e63fd2d319b3816ace6c8d2fc3895c758ee5b
ngu <nguivo9@gmail.com>
2025-09-13 14:06:46 +0100

    refactoring the result page to have each custom widget in a separate page

---
+++ lib/bottom_button.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'constants.dart';
+
+// Custom bottom button widget
+class BottomButton extends StatelessWidget {
+  BottomButton({required this.onTap, required this.buttonTitle});
+
+  final VoidCallback onTap;
+  final String buttonTitle;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        color: kBottomCardColor,
+        margin: EdgeInsets.only(top: 10.0),
+        width: double.infinity,
+        height: kBottomContainerHeight,
+        child: Center(
+          child: Text(
+            buttonTitle,
+            style: kNumberTextStyle,
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/round_icon_button.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'constants.dart';
+
+
+// Custom icon Widget
+class RoundIconButton extends StatelessWidget {
+  RoundIconButton({required this.icon, required this.onPress});
+
+  final IconData icon;
+  final VoidCallback onPress;
+
+  @override
+  Widget build(BuildContext context) {
+    return RawMaterialButton(
+      child: Icon(icon),
+      elevation: 6.0,
+      constraints: BoxConstraints.tightFor(
+        width: 45.0,
+        height: 45.0,
+      ),
+      shape: CircleBorder(),
+      fillColor: Color(0xFF4C4F5E),
+      onPressed: onPress,
+    );
+  }
+}
